ca2a83760e2fb349083d9a007cefedd4eaba1c68
webdevmasum <masumislam0018@gmail.com>
2025-04-08 19:30:29 +0600

    create controller and blade

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('pricing')" :active="request()->routeIs('pricing')">
+                        {{ __('Pricing') }}
+                    </x-nav-link>
                 </div>
             </div>
 
